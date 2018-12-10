eae13ead510e13d8703bb0152e226acd526daa8f
Oleg Osychenko <osychenko@gmail.com>
2018-12-10 22:43:35 +0100

    Initial commit

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-print('hello world')
-print('hello world')
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Oleg Osychenko"
+__copyright__ = "Copyright 2018, Zurich test project"
+__credits__ = ["Zeelo developers"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Oleg Osychenko"
+__email__ = "osychenko@gmail.com"
+__status__ = "Development"
+
+"""Module documentation
+-----------------------
+Requirements:
+    python 3.6 or newer
+Packages:
+    re
+-----------------------
+Description:
+1)...
+2)...
+"""
