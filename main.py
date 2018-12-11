84637d93c8fd7b91b9b9646ba6a56bd24f8ca77c
Oleg Osychenko <osychenko@gmail.com>
2018-12-11 11:11:10 +0100

    Added reading basic Google API and worldcitypop API procs

--- main.py
+++ main.py
@@ -15,10 +15,74 @@ __status__ = "Development"
 Requirements:
     python 3.6 or newer
 Packages:
+    urllib
     re
+    pandas
 -----------------------
 Description:
 1)...
 2)...
 """
-print('new test')
+import urllib.request
+import json
+import pandas as pd
+
+def get_cities_list(country_code="gb"):
+    nrows = 10
+    sort_param = "population"
+    url = f"https://public.opendatasoft.com/api/records/1.0/search/?dataset=worldcitiespop" \
+          f"&rows={nrows}" \
+          f"&sort={sort_param}" \
+          f"&facet=country" \
+          f"&refine.country={country_code}"
+    #request = urllib.request.urlopen(url)
+
+    dummy_output = '{"nhits": 17708, "parameters": {"dataset": ["worldcitiespop"], "refine": {"country": "gb"}, "timezone": "UTC", "rows": 10, "sort": ["population"], "format": "json", "facet": ["country"]}, "records": [{"datasetid": "worldcitiespop", "recordid": "1a3a09c6d9e88e2750844a8155b7bd2f52fd18d4", "fields": {"city": "london", "country": "gb", "region": "H9", "geopoint": [51.514125, -0.093689], "longitude": -0.093689, "latitude": 51.514125, "accentcity": "London", "population": 7421228}, "geometry": {"type": "Point", "coordinates": [-0.093689, 51.514125]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "854dc62297a50ffd882d92fb217ac4d9a4cf4681", "fields": {"city": "birmingham", "country": "gb", "region": "A7", "geopoint": [52.466667, -1.916667], "longitude": -1.916667, "latitude": 52.466667, "accentcity": "Birmingham", "population": 984336}, "geometry": {"type": "Point", "coordinates": [-1.916667, 52.466667]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "f9a3795d645c9a29cfc5622c0a55bea7b439d003", "fields": {"city": "glasgow", "country": "gb", "region": "V2", "geopoint": [55.833333, -4.25], "longitude": -4.25, "latitude": 55.833333, "accentcity": "Glasgow", "population": 610271}, "geometry": {"type": "Point", "coordinates": [-4.25, 55.833333]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "cda05e49431e24c9c96e9d26e50717e2c713a9d7", "fields": {"city": "belfast", "country": "gb", "region": "R3", "geopoint": [54.583333, -5.933333], "longitude": -5.933333, "latitude": 54.583333, "accentcity": "Belfast", "population": 585994}, "geometry": {"type": "Point", "coordinates": [-5.933333, 54.583333]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "c80ec7a8f224c0fbb3aedd1d557b0a96435338df", "fields": {"city": "liverpool", "country": "gb", "region": "H8", "geopoint": [53.416667, -3.0], "longitude": -3.0, "latitude": 53.416667, "accentcity": "Liverpool", "population": 468946}, "geometry": {"type": "Point", "coordinates": [-3.0, 53.416667]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "ee99264d6f1cf1ce5899eff305f94a170a032a40", "fields": {"city": "leeds", "country": "gb", "region": "H3", "geopoint": [53.8, -1.583333], "longitude": -1.583333, "latitude": 53.8, "accentcity": "Leeds", "population": 455124}, "geometry": {"type": "Point", "coordinates": [-1.583333, 53.8]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "08a62eff5ed1a878161e1073263116687bf1eb06", "fields": {"city": "sheffield", "country": "gb", "region": "L9", "geopoint": [53.366667, -1.5], "longitude": -1.5, "latitude": 53.366667, "accentcity": "Sheffield", "population": 447048}, "geometry": {"type": "Point", "coordinates": [-1.5, 53.366667]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "ba9eba66e0fbfebf0b8bb7ffd33557ff77908289", "fields": {"city": "edinburgh", "country": "gb", "region": "U8", "geopoint": [55.95, -3.2], "longitude": -3.2, "latitude": 55.95, "accentcity": "Edinburgh", "population": 435794}, "geometry": {"type": "Point", "coordinates": [-3.2, 55.95]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "0eab24a1db6886503460c5dc0451978359c60894", "fields": {"city": "bristol", "country": "gb", "region": "B7", "geopoint": [51.45, -2.583333], "longitude": -2.583333, "latitude": 51.45, "accentcity": "Bristol", "population": 430714}, "geometry": {"type": "Point", "coordinates": [-2.583333, 51.45]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}, {"datasetid": "worldcitiespop", "recordid": "223f64707d5fc855036cdf4da1867c381c2448b8", "fields": {"city": "manchester", "country": "gb", "region": "I2", "geopoint": [53.5, -2.216667], "longitude": -2.216667, "latitude": 53.5, "accentcity": "Manchester", "population": 395516}, "geometry": {"type": "Point", "coordinates": [-2.216667, 53.5]}, "record_timestamp": "2018-01-08T11:47:45+00:00"}], "facet_groups": [{"name": "country", "facets": [{"name": "gb", "path": "gb", "count": 17708, "state": "refined"}]}]}'
+    json_cities = json.loads(dummy_output)
+    results = json_cities.get("records")
+
+    results_fields = [x["fields"] for x in results]
+    drop_columns = ["country"]
+    print("results=", pd.DataFrame(results_fields).drop(drop_columns, axis=1))
+
+def get_elevation(lat, lng):
+    apikey = "AIzaSyBN_4qldR--d0yErnuOKm9SItD11vIdFEQ"
+    url = "https://maps.googleapis.com/maps/api/elevation/json"
+    request = urllib.request.urlopen(url + "?locations=" + str(lat) + "," + str(lng) + "&key=" + apikey)
+    try:
+        results = json.load(request).get('results')
+        if 0 < len(results):
+            elevation = results[0].get('elevation')
+            # ELEVATION
+            return elevation
+        else:
+            print('HTTP GET Request failed.')
+    except ValueError as e:
+        print('JSON decode failed: ' + str(request))
+
+def get_geo_data():
+    apikey = "AIzaSyASv326cA584q9e707cOiyB_7_guhWdv_4"
+    loc_orig = "Disneyland"
+    loc_dest = "Universal+Studios+Hollywood"
+    transp_mode = "driving" # driving is default
+
+    url = f"https://maps.googleapis.com/maps/api/directions/json?" \
+          f"origin={loc_orig}&destination={loc_dest}" \
+          f"&key={apikey}&mode={transp_mode}"
+    request = urllib.request.urlopen(url)
+
+    try:
+        results = json.load(request).get("results")
+        if 0 < len(results):
+            elevation = results[0].get("elevation")
+            # ELEVATION
+            return elevation
+        else:
+            print("HTTP GET Request failed.")
+    except ValueError as e:
+        print(f"JSON decode failed: {request}")
+        print(f"---\nError output:\n{e}\n---")
+
+if __name__ == '__main__':
+    #print(get_elevation(42.1, 2.1))
+    get_cities_list()
